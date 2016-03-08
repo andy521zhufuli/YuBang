25deea2f9264e4311979e69e3946a17dc11dd1a2
andy521zhufuli <352605880@qq.com>
2016-03-08 19:45:25 +0800

    MyQRCodeActivity

--- app/src/main/java/com/android/andy/yubang/ui/MyQRCodeActivity.java
+++ app/src/main/java/com/android/andy/yubang/ui/MyQRCodeActivity.java
@@ -19,6 +19,7 @@ public class MyQRCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_qrcode);
+
     }
 
 }
